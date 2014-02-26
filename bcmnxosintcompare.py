5e203d94005f0d7934c15bcab5a1e4ac2f457974
Paul Lesiak <palesiak@cisco.com>
2014-02-26 00:10:58 -0500

    Fixed duplicated code

--- bcmnxosintcompare.py
+++ bcmnxosintcompare.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0 
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,98 +29,47 @@ import pprint
 
 intmap = {}
 for line in cli.cli('show interface hardware-mappings').split('\n'):
-	p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<ifIndex>[a-z0-9]+) +(?P<Smod>[0-9]+) +(?P<Unit>[0-9]+) +(?P<HPort>[0-9]+) +(?P<FPort>[0-9]+) +(?P<NPort>[0-9]+) +(?P<VPort>[\-0-9]+)'
-	r = re.match(p, line)
-	if r is not None:
-		if r.groupdict()['mod'] not in intmap:
-			intmap[r.groupdict()['mod']] = []
-		intmap[r.groupdict()['mod']].append(r.groupdict())
+    p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<ifIndex>[a-z0-9]+) +(?P<Smod>[0-9]+) +(?P<Unit>[0-9]+) +(?P<HPort>[0-9]+) +(?P<FPort>[0-9]+) +(?P<NPort>[0-9]+) +(?P<VPort>[\-0-9]+)'
+    r = re.match(p, line)
+    if r is not None:
+        if r.groupdict()['mod'] not in intmap:
+            intmap[r.groupdict()['mod']] = []
+        intmap[r.groupdict()['mod']].append(r.groupdict())
 
 
 for line in cli.cli('show int brief').split('\n'):
-	p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<VLAN>[\-0-9]+) +(?P<type>eth) +(?P<Mode>[a-z]+) +(?P<Status>[a-z]+) +(?P<Reason>.+) +(?P<Speed>[a-zA-Z0-9]+\(.\)) +(?P<PortCh>[\-0-9]+)'
-	r = re.match(p, line)
-	if r is not None:
-		if r.groupdict()['mod'] not in intmap:
-			raise 'Found module in show brief that wasn\'t found in show interface hardware-mappings'
-		for interface in intmap[r.groupdict()['mod']]:
-			if interface['interface'] == r.groupdict()['interface']:
-				for k,v in r.groupdict().items():
-					if k not in interface:
-						interface[k] = v
+    p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<VLAN>[\-0-9]+) +(?P<type>eth) +(?P<Mode>[a-z]+) +(?P<Status>[a-z]+) +(?P<Reason>.+) +(?P<Speed>[a-zA-Z0-9]+\(.\)) +(?P<PortCh>[\-0-9]+)'
+    r = re.match(p, line)
+    if r is not None:
+        if r.groupdict()['mod'] not in intmap:
+            raise 'Found module in show brief that wasn\'t found in show interface hardware-mappings'
+        for interface in intmap[r.groupdict()['mod']]:
+            if interface['interface'] == r.groupdict()['interface']:
+                for k, v in r.groupdict().items():
+                    if k not in interface:
+                        interface[k] = v
 
 
-for mod,interfaces in intmap.items():
-	bcmmap = {}
-	bcmPortIndex = -1
-	for interface in interfaces:
-		bcmPortIndex += 1
-		if interface['Unit'] not in bcmmap:
-			bcmPortIndex = -1
-			bcmmap[interface['Unit']] = []
-			for line in libbcmshell.runBcmCmd(int(mod), int(interface['Unit']), 'ps').split('\n'):
-				p = '^ +(?P<bcmPort>xe[0-9]+) +(?P<bcmEnalink>[\!a-z]+) +(?P<bcmSpeed>[0-9A-Z]+) +(?P<bcmDuplex>[A-Z]+) +(?P<bcmLinkScan>[A-Z]+) +(?P<bcmAutoNeg>Yes|No) +(?P<bcmSTPState>Forward|Disable) +(?P<bcmPause>[ TRX]+) +(?P<bcmDiscard>None) +(?P<bcmLrnOps>[A-Z]+) +(?P<bcmInterface>[A-Z0-9]+) +(?P<bcmMaxFrame>[0-9]+).*'
-				r = re.match(p, line)
-				if r is not None:
-					d = r.groupdict()
-					bcmmap[interface['Unit']].append(d)
-		else:
-			print 'found ' + interface['Unit']
-		for k,v in bcmmap[interface['Unit']][bcmPortIndex].items():
-			if k not in interface:
-				interface[k] = v
+for mod, interfaces in intmap.items():
+    bcmmap = {}
+    bcmPortIndex = -1
+    for interface in interfaces:
+        bcmPortIndex += 1
+        if interface['Unit'] not in bcmmap:
+            bcmPortIndex = -1
+            bcmmap[interface['Unit']] = []
+            for line in libbcmshell.runBcmCmd(int(mod), int(interface['Unit']), 'ps').split('\n'):
+                p = '^ +(?P<bcmPort>xe[0-9]+) +(?P<bcmEnalink>[\!a-z]+) +(?P<bcmSpeed>[0-9A-Z]+) +(?P<bcmDuplex>[A-Z]+) +(?P<bcmLinkScan>[A-Z]+) +(?P<bcmAutoNeg>Yes|No) +(?P<bcmSTPState>Forward|Disable) +(?P<bcmPause>[ TRX]+) +(?P<bcmDiscard>None) +(?P<bcmLrnOps>[A-Z]+) +(?P<bcmInterface>[A-Z0-9]+) +(?P<bcmMaxFrame>[0-9]+).*'
+                r = re.match(p, line)
+                if r is not None:
+                    d = r.groupdict()
+                    bcmmap[interface['Unit']].append(d)
+        else:
+            print 'found ' + interface['Unit']
+        for k, v in bcmmap[interface['Unit']][bcmPortIndex].items():
+            if k not in interface:
+                interface[k] = v
 
-import cli
-import libbcmshell
-import re
-import pprint
-
-intmap = {}
-for line in cli.cli('show interface hardware-mappings').split('\n'):
-	p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<ifIndex>[a-z0-9]+) +(?P<Smod>[0-9]+) +(?P<Unit>[0-9]+) +(?P<HPort>[0-9]+) +(?P<FPort>[0-9]+) +(?P<NPort>[0-9]+) +(?P<VPort>[\-0-9]+)'
-	r = re.match(p, line)
-	if r is not None:
-		if r.groupdict()['mod'] not in intmap:
-			intmap[r.groupdict()['mod']] = []
-		intmap[r.groupdict()['mod']].append(r.groupdict())
-
-
-for line in cli.cli('show int brief').split('\n'):
-	p = '^Eth(?P<mod>[0-9]+)\/(?P<interface>[0-9\/]+) +(?P<VLAN>[\-0-9]+) +(?P<type>eth) +(?P<Mode>[a-z]+) +(?P<Status>[a-z]+) +(?P<Reason>.+) +(?P<Speed>[a-zA-Z0-9]+\(.\)) +(?P<PortCh>[\-0-9]+)'
-	r = re.match(p, line)
-	if r is not None:
-		if r.groupdict()['mod'] not in intmap:
-			raise 'Found module in show brief that wasn\'t found in show interface hardware-mappings'
-		for interface in intmap[r.groupdict()['mod']]:
-			if interface['interface'] == r.groupdict()['interface']:
-				for k,v in r.groupdict().items():
-					if k not in interface:
-						interface[k] = v
-
-
-for mod,interfaces in intmap.items():
-	bcmmap = {}
-	bcmPortIndex = -1
-	for interface in interfaces:
-		bcmPortIndex += 1
-		if interface['Unit'] not in bcmmap:
-			bcmPortIndex = 0
-			bcmmap[interface['Unit']] = []
-			for line in libbcmshell.runBcmCmd(int(mod), int(interface['Unit']), 'ps').split('\n'):
-				p = '^ +(?P<bcmPort>xe[0-9]+) +(?P<bcmEnalink>[\!a-z]+) +(?P<bcmSpeed>[0-9A-Z]+) +(?P<bcmDuplex>[A-Z]+) +(?P<bcmLinkScan>[A-Z]+) +(?P<bcmAutoNeg>Yes|No) +(?P<bcmSTPState>Forward|Disable) +(?P<bcmPause>[ TRX]+) +(?P<bcmDiscard>None) +(?P<bcmLrnOps>[A-Z]+) +(?P<bcmInterface>[A-Z0-9]+) +(?P<bcmMaxFrame>[0-9]+).*'
-				r = re.match(p, line)
-				if r is not None:
-					d = r.groupdict()
-					bcmmap[interface['Unit']].append(d)
-		else:
-			print 'found ' + interface['Unit']
-		for k,v in bcmmap[interface['Unit']][bcmPortIndex].items():
-			if k not in interface:
-				interface[k] = v
-
-for mod,interfaces in intmap.items():
-	for interface in interfaces:
-		print 'interface Eth%s/%s : BCM %s/%s/%s :: %s : %s' % (mod, interface['interface'], mod, interface['Unit'],interface['bcmPort'] , interface['Status'], interface['bcmEnalink'])
-
-
-
+for mod, interfaces in intmap.items():
+    for interface in interfaces:
+        print 'interface Eth%s/%s : BCM %s/%s/%s :: %s : %s' % (mod, interface['interface'], mod, interface['Unit'], interface['bcmPort'], interface['Status'], interface['bcmEnalink'])
